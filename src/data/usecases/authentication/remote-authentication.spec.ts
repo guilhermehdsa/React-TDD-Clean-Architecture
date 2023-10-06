517de36069f30608a716c9922a983c6734b7e6bd
GuilhermeHDSA <Guilhermepb00@gmail.com>
2023-10-06 17:55:28 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,7 @@
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "@/data/test/mock-http-client"
+import { InvalidCredentialsError } from "@/domain/errors/InvalidCredentialsError";
 import { mockAuthentication } from "@/domain/test/mock-authentication";
 
 import { faker } from '@faker-js/faker';
@@ -31,6 +33,15 @@ describe("RemoteAuthentication",() => {
         const authenticationParams = mockAuthentication()
         await sut.auth(authenticationParams)
         expect(httpPostClientSpy.body).toEqual(authenticationParams)
+    })
+
+    test('Should throw invalidCredentialsError if HttpPostClient returns 401 ', async () => {
+        const {sut, httpPostClientSpy } = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unauthorized
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError)
     })
 })
 
